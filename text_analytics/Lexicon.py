cb3f8b5e44cdd06eb660f105c7fa614c12045755
John Gugliotti <john.gugliotti@gmail.com>
2012-04-02 06:53:51 -0400

    tested cosine similarity, random opinion

--- text_analytics/Lexicon.py
+++ text_analytics/Lexicon.py
@@ -1,4 +1,7 @@
 import random
+import math
+import Opinion
+import csv
 
 class SemanticClass(object) :
 	"""terms definining a point of view"""
@@ -10,53 +13,223 @@ class SemanticClass(object) :
 		
 class Lexicon(object) :
 	"""object to encapsulate POVs"""
-	def __init__(self) :
+	def __init__(self,cloudsize=20) :
+		self.cloudsize=cloudsize
 		self._lexi={}
-		self._lexi['republican']=["constutionality", "cuttaxes", "secondamendment", "freeenterprise", "smallgovernment", "religiousfreedom", "drillherenow", "oil", "overturnroevswade", "prochoice" , 'rep1' ,'rep2' ,'rep3']
-		self._lexi['democrat']=["taxthewealthy", "equalrights", "economicequality", "closeguantanamo", "epa", "nodrillinginwildlife", "alternativeenergy", "repealDNDT" ,'dem1','dem2','dem3','dem4']
-		self._lexi['middle']=['energyindepdendence', 'jobs', 'economicgrowth', 'smallbusiness', 'education', 'middle1' ,'middle2' ,'middle3', 'middle4', 'middle5']
+
+		#self._lexi['republican']=["constutionality", "cuttaxes", "secondamendment", "freeenterprise", "smallgovernment", "religiousfreedom", "drillherenow", "domesticoil", "overturnroevswade", "prochoice" , 'rep1' ,'rep2' ,'rep3']
+		#self._lexi['democrat']=["taxthewealthy", "equalrights", "economicequality", "closeguantanamo", "epa", "nodrillinginwildlife", "alternativeenergy", "repealDNDT" ,'dem1','dem2','dem3','dem4']
+		#self._lexi['middle']=['energyindepdendence', 'jobs', 'economicgrowth', 'smallbusiness', 'education', 'middle1' ,'middle2' ,'middle3', 'middle4', 'middle5']
+		
+		width=2.0/cloudsize/2
+		
+		self.inter_document_frequency={}
+		
+		#{category : {token : value}}
+		self._lexi['republican']={}
+		self._lexi['democrat']={}
+		for i in range(0,cloudsize+1) :
+			r1=('r' + str(i))
+			d1=('d' + str(i))
+			self._lexi['republican'][r1]= (-width)
+			self._lexi['democrat'][('d' + str(i))]= (+width)
+			self.inter_document_frequency[r1]=0.0
+			self.inter_document_frequency[d1]=0.0
 		
 		self._lexi_lookup={}
 		self._randomorder=[]
+		#reverse lookup
+		# {term : category}
 		for key in self._lexi :
 			for value in self._lexi[key] :
 				self._lexi_lookup[value]=key
 				self._randomorder.append(value)
-		self.shuffle_lexicon()	
+		self.shuffle_lexicon()
+	
+	def get_idf_vector(self) : return self.inter_document_frequency
+	def clear_idf_vector(self) : self.inter_document_frequency = {}
+	
+	
+	
+	def cosine_distance(self,vector1={},vector2={}) :
+		"""
+			Determine the cosine distance between two term-term_frequency vectors
+				cosine_distance(dict,dict) --> float in [0,1]
+				
+				Keyword arguments:
+				vector1 , vector2 - dict object containing kv pairs of the form {term : term_frequency}
+		"""
+		tf_idf_1={}
+		tf_idf_2={}
+		#calculate term frequencies for each document
+		for t in vector1 :
+			if t not in tf_idf_1 : tf_idf_1[t]=0
+			tf_idf_1[t]+=1
+		for t in vector2 :
+			if t not in tf_idf_2 : tf_idf_2[t]=0
+			tf_idf_2[t]+=1
+		#calculate tf_idf vector values for each document
+		for t in tf_idf_1 :
+			if self.inter_document_frequency[t]==0.0 :
+				tf_idf_1[t]= 0.0
+			else :
+				tf_idf_1[t]/self.inter_document_frequency[t]
+				
+		for t in tf_idf_2 :
+			if self.inter_document_frequency[t]==0.0 :
+				tf_idf_2[t]= 0.0
+			else :
+				tf_idf_2[t]/self.inter_document_frequency[t]
+		ks1=set(tf_idf_1.keys())
+		ks2=set(tf_idf_2.keys())
+		#print str(sorted(ks1))
+		#print str(sorted(ks2))
+		keyset=ks1.intersection(ks2)
+		#print str(keyset)
+		#print 'intersection ' + str(len(keyset))
+		#print 'union ' + str(len((set(tf_idf_1.keys()).union(set(tf_idf_2.keys())))))
 		
-	def politicalization_matrix(self,vector=[]) :
+		dp=0.0
+		m1=0.0
+		m2=0.0
+		ci=0
+		for k in keyset :
+			#print str(k)
+			dp+=(tf_idf_2[k]*tf_idf_1[k])
+			ci+=1
+		#print str(len(keyset)) + ' items in common, len of .product: ' + str(ci)
+		ci=0
+		for t in tf_idf_1 :
+			m1+=math.pow(tf_idf_1[t],2.0)
+			ci+=1
+		#print 'len of m1: ' + str(ci) + str(sorted(list(tf_idf_1.values())))
+		ci=0
+		for t in tf_idf_2 :
+			m2+=math.pow(tf_idf_2[t],2.0)
+			ci+=1
+		#print 'len of m2: ' + str(ci)
+		
+		#print 'dotprod=' + str(dp) + ' ' +  'm1=' + ' ' + str(m1) + ' ' +  'm2=' + str(m2)
+
+		return (dp / (math.sqrt(m1) * math.sqrt(m2)))
+
+	def jaccard_distance(self,vector1=[],vector2=[]) :
+		"""calculates distance between vectors based on intersection and union sizes"""
+		s1=set(vector1)
+		s2=set(vector2)
+		intersect=s1.intersection(s2) 
+		union=s1.union(s2)
+		return 1.0-(1.0*len(intersect)/len(union)*1.0)
+
+
+	def politicalization_matrix(self,vector={}) :
 		rep=0
 		dem=0
 		mid=0
 		for v in vector :
-			if self._lexi_lookup[v] == "republican" : rep+=1
-			if self._lexi_lookup[v] == "democrat" : dem+=1
-			if self._lexi_lookup[v] == "middle" : mid+=1
-			
+			if self._lexi_lookup[vector[v]] == "republican" : rep+=1
+			if self._lexi_lookup[vector[v]] == "democrat" : dem+=1
+			if self._lexi_lookup[vector[v]] == "middle" : mid+=1
 		return rep,dem,mid
-	
-	def numericalize_opinion(self,vector=[]) :
-		rep,dem,mid=self.politicalization_matrix(vector)
-		#print str((rep,dem,mid))
-		ttl=1.0*rep+dem
-		m = 1.5 if mid == 0 else 1.0 / mid
-		
-		s_rep=0 if ttl==0 else rep/ttl
-		
-		s_dem= 0 if ttl == 0 else dem/ttl
-		#print str((s_rep,s_dem,m))
-		return (m-(s_rep-s_dem)) if s_rep > s_dem else (m + (s_dem-s_rep))
-		
 	
 	def shuffle_lexicon(self) :
 		random.shuffle(self._randomorder)
 		
-	def get_random_opinion(self, wordcount=0) :
+	def get_skewed_opinion(self,skew=0.0) :
+		sentiment=[]
+		nr_pos=0
+		nr_neg=0
+		if skew>0 :
+			nr_neg= int(math.fabs(skew-1)*self.cloudsize)
+			nr_pos=self.cloudsize-nr_neg
+		if skew == 0 :
+			nr_pos=self.cloudsize/2
+			nr_neg=self.cloudsize/2
+		if skew <0 :
+			nr_pos=int(math.fabs(skew+1)*self.cloudsize)
+			nr_neg=self.cloudsize-nr_pos
+		#print str(nr_neg) + ' / ' + str(nr_pos)
+		rep=[ t.lower() for t in self._lexi['republican']]
+		dem=[ t.lower() for t in self._lexi['democrat']]
+		random.shuffle(rep)	
+		random.shuffle(dem)
+		sentiment.extend(rep[0:nr_neg])
+		sentiment.extend(dem[0:nr_pos])
+		#add term frequencies to the IDF
+		for t in sentiment : self.inter_document_frequency[t]+=1
+		random.shuffle(sentiment)
+		return sentiment
+		
+	def get_random_opinion(self, wordcount=20) :
+		"""
+		get_random_opinion(int) --> dict
+		"""
 		self.shuffle_lexicon()
-		output=[]
-		output.extend(self._randomorder[0:wordcount-1])
+		output={}
+		for i in range(0,wordcount+1) :
+			rint=random.randint(0,len(self._randomorder)-1)
+			print str(rint) + ' ' + str(len(self._randomorder))
+			t = self._randomorder[rint]    #don't care about duplicates
+			if t not in output : output[t]=0.0
+			output[t]+=1
+			self.inter_document_frequency[t]+=1
 		return output
+
+	def get_numeric_position(self,wordcloud=[]) :
+		position=0.0
+		cat_neg='republican'
+		nr_neg=0
+		cat_pos='democrat'
+		nr_pos=0
+		for w in wordcloud :
+			category=self._lexi_lookup[w]
+			if category == 'republican' :
+				nr_neg+=1
+			else :
+				nr_pos+=1
+		position = 1.0*nr_neg / (nr_neg+nr_pos)
+		return position
+
+def write_vector(vector={},outputfilename=None,outputfile=None) :
+	if outputfilename == None and outputfile == None : return
+	fout=None
+	if outputfilename != None :
+		fout=open(outputfilename,'a')
+		print 'opening new file in append mode'
+	if outputfile != None :
+		fout=outputfile
+		print 'using given output file'
+	writer=csv.writer(fout)
+	for t in vector : 
+		writer.writerow([t,vector[t]])
+
+	if outputfilename != None : fout.close()
+
+def test01() :
+	L = Lexicon(40)
+	opinions=[]
+	for i in range(0,20)  : opinions.append(L.get_random_opinion(10))
+	idf=L.get_idf_vector()
+	fout=open('/Users/johngugliotti/Dev/pydev/text_analytics/test01','wb')
+	write_vector(idf,outputfile=fout)
+	write_vector({'--':'--'},outputfile=fout)
+	write_vector(opinions[2],outputfile=fout)
+	a=list(sorted(opinions[2].keys()))
+	b=list(sorted(opinions[1].keys()))
+	print str(a)
+	print str(b)
+	print str(L.cosine_distance(opinions[1],opinions[2]))
 	
 	
-		
-		
+	write_vector({'--':'--'},outputfile=fout)
+	write_vector(opinions[1],outputfile=fout)
+	write_vector({'--':'--'},outputfile=fout)
+	write_vector({'o1-->o2': L.cosine_distance(opinions[1],opinions[2])},outputfile=fout)
+	fout.close()
+	
+	for i in range(0,len(opinions)-1) :
+		for j in range(i+1,len(opinions)-1) :
+			print str(i) + ' ' + str(j) + ':  ' + str(L.cosine_distance(opinions[i],opinions[j]))
+	
+	
+	
